fd5ef9f61517958489d1029c545e7551d612b77d
Sousa Dany <danysousa.sd@gmail.com>
2015-03-04 10:59:04 +0100

    forget

--- includes/CoreEngine.hpp
+++ includes/CoreEngine.hpp
@@ -6,7 +6,7 @@
 /*   By: dsousa <dsousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/03 13:59:51 by dsousa            #+#    #+#             */
-/*   Updated: 2015/03/03 15:20:25 by dsousa           ###   ########.fr       */
+/*   Updated: 2015/03/04 10:58:51 by dsousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,11 @@ class CoreEngine
 		** OPERATOR
 		*/
 		CoreEngine operator=( CoreEngine const & cpy );
+
+		/*
+		** METHOD
+		*/
+		void			loop(void);
 
 	private:
 		GameEngine		*gameEngine;
