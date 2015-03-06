bc8e24f4b7178c62d958c7b118fc781f112fef25
nschilli <schilling.norman@hotmail.fr>
2015-03-06 10:43:43 +0100

    ~check params main.cpp

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,13 +6,14 @@
 /*   By: nschilli <nschilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/03 13:23:20 by dsousa            #+#    #+#             */
-/*   Updated: 2015/03/06 10:19:22 by nschilli         ###   ########.fr       */
+/*   Updated: 2015/03/06 10:40:55 by nschilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <cctype>
 #include <CoreEngine.hpp>
 
 static void		ft_usage( void )
@@ -22,6 +23,19 @@ static void		ft_usage( void )
 	return ;
 }
 
+static int		ft_check_params( char *str )
+{
+	for ( int i = 0;str[i] != '\0';i++ )
+	{
+		if ( !isdigit( str[i] ) )
+		{
+			ft_usage();
+			return ( 1 );
+		}
+	}
+	return ( 0 );
+}
+
 int main( int argc, char **argv )
 {
 	srand(time(NULL));
@@ -29,11 +43,12 @@ int main( int argc, char **argv )
 		ft_usage();
 	else
 	{
-		CoreEngine	core = CoreEngine( static_cast<int>( atoi(argv[1]) ), static_cast<int>( atoi(argv[2]) ) );
-		core.loop();
+		if ( ft_check_params( argv[1] ) == 0 && ft_check_params( argv[2] ) == 0 )
+		{
+			CoreEngine	core = CoreEngine( static_cast<int>( atoi(argv[1]) ), static_cast<int>( atoi(argv[2]) ) );
+			core.loop();
+		}
 	}
-
-
 
 	return 0;
 }
